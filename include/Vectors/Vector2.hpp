9795b1fa030a507646dfc9fa19a049c363ccbf30
Loïc Pottier <loic.pottier@live.fr>
2023-12-21 10:46:13 +0100

    test GLFW LIB

---
+++ include/Vectors/Vector2.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cmath>
+
+class Vector2 {
+public:
+  double x, y;
+
+  Vector2();
+  Vector2(double x, double y);
+
+  Vector2 operator+(const Vector2 &other);
+  Vector2 operator-(const Vector2 &other);
+  Vector2 operator*(const Vector2 &other);
+  Vector2 operator/(const Vector2 &other);
+
+  Vector2 operator+=(const Vector2 &other);
+  Vector2 operator-=(const Vector2 &other);
+  Vector2 operator*=(const Vector2 &other);
+  Vector2 operator/=(const Vector2 &other);
+
+  bool operator==(const Vector2 &other);
+  bool operator!=(const Vector2 &other);
+
+  double distance(Vector2 &other);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,34 @@
-#include <SFML/Graphics.hpp>
-#include <imgui.h>
-#include <imgui-sfml.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-#include <GL/glew.h>
+int main(void) {
+  std::cout << "Starting SPACE..." << std::endl;
+  GLFWwindow* window;
 
-int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Space Engine");
-  glewInit();
-  ImGui::SFML::Init(window);
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
-
-      ImGui::SFML::Update(window);
-    }
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    ImGui::Begin("Space engine");
-
-    ImGui::End();
-    window.display();
+  if (!glfwInit()) {
+    std::cerr << "Error initalizing GLFW" << std::endl;
+    return -1;
   }
 
-  ImGui::SFML::Shutdown();
-  window.close();
+  window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
 
+  glfwMakeContextCurrent(window);
+  glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+
+  while (!glfwWindowShouldClose(window)) {
+    /* Render */
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
