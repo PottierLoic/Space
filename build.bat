b7345ca2150b71842e38d77fecb8b38c22666bae
Loïc Pottier <loic.pottier@live.fr>
2023-12-21 12:31:34 +0100

    glfw working ?

--- build.bat
+++ build.bat
@@ -1,5 +1,13 @@
-cmake -DBUILD_SHARED_LIBS=OFF -G "MinGW Makefiles" -B "./build" . -DFLAGS=%1
 @echo off
+copy /Y .\libs\imgui\backends\imgui_impl_glfw.cpp .\libs\imgui
+copy /Y .\libs\imgui\backends\imgui_impl_glfw.h .\libs\imgui
+copy /Y .\libs\imgui\backends\imgui_impl_opengl3.cpp .\libs\imgui
+copy /Y .\libs\imgui\backends\imgui_impl_opengl3.h .\libs\imgui
+copy /Y .\libs\imgui\backends\imgui_impl_opengl3_loader.h .\libs\imgui
+
+
+cmake -DBUILD_SHARED_LIBS=OFF -G "MinGW Makefiles" -B "./build" . -DFLAGS=%1
+
 cd build
 make
 cd ..
