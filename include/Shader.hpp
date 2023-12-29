1735f114b06f4afa59e55bcc7ae00b2b251440f5
Loïc Pottier <loic.pottier@live.fr>
2023-12-29 14:02:30 +0100

    camera movement and mouse control

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -6,10 +6,11 @@
 #include <iostream>
 
 #include "glad/glad.h"
+#include "glm/glm.hpp"
 
 class Shader {
 public:
-  unsigned int id;    // Program ID
+  unsigned int id;    // Program id
 
   // Constructor reads and builds the shader
   Shader(const char* vertexPath, const char* fragmentPath);
@@ -20,4 +21,13 @@ public:
   void setBool(const std::string& name, bool value) const;
   void setInt(const std::string& name, int value) const;
   void setFloat(const std::string& name, float value) const;
+  void setVec2(const std::string &name, const glm::vec2 &value) const;
+  void setVec2(const std::string &name, float x, float y) const;
+  void setVec3(const std::string &name, const glm::vec3 &value) const;
+  void setVec3(const std::string &name, float x, float y, float z) const;
+  void setVec4(const std::string &name, const glm::vec4 &value) const;
+  void setVec4(const std::string &name, float x, float y, float z, float w) const;
+  void setMat2(const std::string &name, const glm::mat2 &mat) const;
+  void setMat3(const std::string &name, const glm::mat3 &mat) const;
+  void setMat4(const std::string &name, const glm::mat4 &mat) const;
 };
