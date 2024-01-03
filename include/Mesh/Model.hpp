383e930b2d90cb2d962c42122b62d805bb56ab64
Loïc Pottier <loic.pottier@live.fr>
2024-01-03 12:00:12 +0100

    model loading work

--- include/Mesh/Model.hpp
+++ include/Mesh/Model.hpp
@@ -1,30 +1,32 @@
-// #pragma once
+#pragma once
 
-// #include <vector>
-// #include <string>
+#include <vector>
+#include <string>
 
-// #include "assimp/Importer.hpp"
-// #include "assimp/scene.h"
-// #include "assimp/postprocess.h"
-// #include "stb_image.h"
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
+#include "stb_image.h"
 
-// #include "Mesh/Mesh.hpp"
-// #include "Mesh/Texture.hpp"
-// #include "Shader.hpp"
+#include "Mesh/Mesh.hpp"
+#include "Mesh/Texture.hpp"
+#include "Shader.hpp"
 
-// unsigned int textureFromFile(const char *path, const std::string &directory, bool gamma = false);
+unsigned int textureFromFile(const char *path, const std::string &directory, bool gamma = false);
 
-// class Model {
-// public:
-//   Model(char *path);
-//   void draw(Shader &shader);
+class Model {
+public:
+  Model(const char *path);
+  void draw(Shader &shader);
 
-// private:
-//   std::vector<Mesh> meshes;
-//   std::string directory;
+private:
+  std::vector<Texture> textures_loaded;
 
-//   void loadModel(std::string path);
-//   void processNode(aiNode *node, const aiScene *scene);
-//   Mesh processMesh(aiMesh *mesh, const aiScene *scene);
-//   std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
-// };
+  std::vector<Mesh> meshes;
+  std::string directory;
+
+  void loadModel(std::string path);
+  void processNode(aiNode *node, const aiScene *scene);
+  Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+  std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
+};
