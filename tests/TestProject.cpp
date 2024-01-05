83f333b5dd26a1c0ac115642b065a6ade0586ec3
Loïc Pottier <loic.pottier@live.fr>
2024-01-05 17:27:37 +0100

    Project folder creation structure and testing

---
+++ tests/TestProject.cpp
@@ -0,0 +1,11 @@
+#include "Test.hpp"
+#include "Project.hpp"
+
+int testProject() {
+  std::filesystem::path currentPath = __FILE__;
+  std::filesystem::path sourceDir = currentPath.parent_path();
+
+   Project test((sourceDir / "Generated").string(), "test");
+  /* Distance check */
+  return 0;
+}
