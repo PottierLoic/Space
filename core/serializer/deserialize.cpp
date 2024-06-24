8a57f31d211783f8014a2cfd1e56937758658c23
Loïc Pottier <loic.pottier@live.fr>
2024-06-24 15:32:13 +0200

    fix(Serializer): correct pointer use to build on mac

--- core/serializer/deserialize.cpp
+++ core/serializer/deserialize.cpp
@@ -2,17 +2,17 @@
 
 namespace SpaceEngine {
 
-std::shared_ptr<Scene> Serializer::deserializeScene(const json& j) {
+std::shared_ptr<Scene> Serializer::deserializeScene(const json &j) {
   std::shared_ptr<Scene> scene = std::make_shared<Scene>();
   scene->entities.pop_back();
   scene->name = j["name"];
-  for (const auto& entityJson : j["entities"]) {
+  for (const auto &entityJson : j["entities"]) {
     auto entity = deserializeEntity(entityJson);
     if (entity) {
       scene->entities.push_back(entity);
     }
   }
-  for (const auto& entity : scene->entities) {
+  for (const auto &entity : scene->entities) {
     if (entity->getComponent<Camera>()) {
       scene->selectedCamera = entity->getComponent<Camera>();
       break;
@@ -21,16 +21,18 @@ std::shared_ptr<Scene> Serializer::deserializeScene(const json& j) {
   return scene;
 }
 
-std::shared_ptr<Entity> Serializer::deserializeEntity(const json& j) {
+std::shared_ptr<Entity> Serializer::deserializeEntity(const json &j) {
   std::shared_ptr<Entity> ent = Entity::createEmpty();
-  for (const auto& compJson : j["components"]) {
+  for (const auto &compJson : j["components"]) {
     std::shared_ptr<Component> component = deserializeComponent(compJson);
     if (component) {
-      std::type_index typeIndex(typeid(*component));
+      auto &dereferencedComponent = *component;
+      std::type_index typeIndex(typeid(dereferencedComponent));
       ent->components[typeIndex] = component;
     }
   }
   return ent;
 }
 
-}
+} // namespace SpaceEngine
+
--- core/serializer/serialize_component.cpp
+++ core/serializer/serialize_component.cpp
@@ -2,49 +2,58 @@
 
 namespace SpaceEngine {
 
-std::unordered_map<std::type_index, std::function<json(const std::shared_ptr<Component>&)>> Serializer::serializers = []() {
-  std::unordered_map<std::type_index, std::function<json(const std::shared_ptr<Component>&)>> m;
-  // Transform
-  m[std::type_index(typeid(Transform))] = [](const std::shared_ptr<Component>& comp) -> json {
-    auto tf = std::static_pointer_cast<Transform>(comp);
-    json j;
-    j["type"] = "Transform";
-    j["name"] = tf->name;
-    j["position"] = { tf->position.x(), tf->position.y(), tf->position.z() };
-    j["rotation"] = { tf->rotation.x(), tf->rotation.y(), tf->rotation.z() };
-    j["scale"] = { tf->scale.x(), tf->scale.y(), tf->scale.z() };
-    return j;
-  };
-  // Camera
-  m[std::type_index(typeid(Camera))] = [](const std::shared_ptr<Component>& comp) -> json {
-    auto cam = std::static_pointer_cast<Camera>(comp);
-    json j;
-    j["type"] = "Camera";
-    j["front"] = { cam->front.x, cam->front.y, cam->front.z };
-    j["up"] = { cam->up.x, cam->up.y, cam->up.z };
-    j["right"] = { cam->right.x, cam->right.y, cam->right.z };
-    j["worldUp"] = { cam->worldUp.x, cam->worldUp.y, cam->worldUp.z };
-    j["yaw"] = cam->yaw;
-    j["pitch"] = cam->pitch;
-    j["zoom"] = cam->zoom;
-    j["skyboxColor"] = { cam->skyboxColor.x, cam->skyboxColor.y, cam->skyboxColor.z, cam->skyboxColor.w };
-    return j;
-  };
-  // ModelRenderer
-  m[std::type_index(typeid(ModelRenderer))] = [](const std::shared_ptr<Component>& comp) -> json {
-    auto modelRenderer = std::static_pointer_cast<ModelRenderer>(comp);
-    json j;
-    j["type"] = "ModelRenderer";
-    j["path"] = modelRenderer->path;
-    return j;
-  };
-  // TODO: Add other serializers for all components.
-  return m;
-}();
-
+std::unordered_map<std::type_index,
+                   std::function<json(const std::shared_ptr<Component> &)>>
+    Serializer::serializers = []() {
+      std::unordered_map<
+          std::type_index,
+          std::function<json(const std::shared_ptr<Component> &)>>
+          m;
+      // Transform
+      m[std::type_index(typeid(Transform))] =
+          [](const std::shared_ptr<Component> &comp) -> json {
+        auto tf = std::static_pointer_cast<Transform>(comp);
+        json j;
+        j["type"] = "Transform";
+        j["name"] = tf->name;
+        j["position"] = {tf->position.x(), tf->position.y(), tf->position.z()};
+        j["rotation"] = {tf->rotation.x(), tf->rotation.y(), tf->rotation.z()};
+        j["scale"] = {tf->scale.x(), tf->scale.y(), tf->scale.z()};
+        return j;
+      };
+      // Camera
+      m[std::type_index(typeid(Camera))] =
+          [](const std::shared_ptr<Component> &comp) -> json {
+        auto cam = std::static_pointer_cast<Camera>(comp);
+        json j;
+        j["type"] = "Camera";
+        j["front"] = {cam->front.x, cam->front.y, cam->front.z};
+        j["up"] = {cam->up.x, cam->up.y, cam->up.z};
+        j["right"] = {cam->right.x, cam->right.y, cam->right.z};
+        j["worldUp"] = {cam->worldUp.x, cam->worldUp.y, cam->worldUp.z};
+        j["yaw"] = cam->yaw;
+        j["pitch"] = cam->pitch;
+        j["zoom"] = cam->zoom;
+        j["skyboxColor"] = {cam->skyboxColor.x, cam->skyboxColor.y,
+                            cam->skyboxColor.z, cam->skyboxColor.w};
+        return j;
+      };
+      // ModelRenderer
+      m[std::type_index(typeid(ModelRenderer))] =
+          [](const std::shared_ptr<Component> &comp) -> json {
+        auto modelRenderer = std::static_pointer_cast<ModelRenderer>(comp);
+        json j;
+        j["type"] = "ModelRenderer";
+        j["path"] = modelRenderer->path;
+        return j;
+      };
+      // TODO: Add other serializers for all components.
+      return m;
+    }();
 
 json Serializer::serialize(const std::shared_ptr<Component> component) {
-  auto typeIndex = std::type_index(typeid(*component));
+  auto &dereferencedComponent = *component;
+  auto typeIndex = std::type_index(typeid(dereferencedComponent));
   auto it = serializers.find(typeIndex);
   if (it != serializers.end()) {
     return it->second(component);
@@ -52,4 +61,4 @@ json Serializer::serialize(const std::shared_ptr<Component> component) {
   return json{};
 }
 
-}
+} // namespace SpaceEngine
