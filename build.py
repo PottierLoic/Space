0f0b7767bd96466ecfc2f6e9ace77be52cbdea6d
Loïc Pottier <loic.pottier@live.fr>
2025-05-09 09:21:02 -0700

    fix(build): add flags to cmake_command

--- build.py
+++ build.py
@@ -2,8 +2,8 @@ import os
 import shutil
 import subprocess
 
-subprocess.run(['git', 'submodule', 'init'])
-subprocess.run(['git', 'submodule', 'update'])
+# subprocess.run(['git', 'submodule', 'init'])
+# subprocess.run(['git', 'submodule', 'update'])
 
 shutil.copy2('./libs/imgui/backends/imgui_impl_glfw.cpp', './libs/imgui')
 shutil.copy2('./libs/imgui/backends/imgui_impl_glfw.h', './libs/imgui')
@@ -18,7 +18,16 @@ if os.path.exists(generated_tests_path):
   shutil.rmtree(generated_tests_path)
 
 if os.name == 'nt':
-  cmake_command = ['cmake', '-DBUILD_SHARED_LIBS=OFF', '-G', 'MinGW Makefiles', '-B', './build', '.']
+  cmake_command = [
+    'cmake',
+    '-DBUILD_SHARED_LIBS=OFF',
+    '-G', 'MinGW Makefiles',
+    '-DCMAKE_MAKE_PROGRAM=make',
+    '-DCMAKE_C_COMPILER=gcc',
+    '-DCMAKE_CXX_COMPILER=g++',
+    '-B', './build',
+    '.'
+  ]
   make_command = ['make', '-C', './build']
 else:
   cmake_command = ['cmake', '-DBUILD_SHARED_LIBS=OFF', '-G', 'Unix Makefiles', '-B', './build', '.']
