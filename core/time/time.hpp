5188b23a0f7cf160fe6e65f929795865fef626c3
Loïc Pottier <loic.pottier@live.fr>
2025-05-12 13:40:23 -0700

    feat(core): define `time` class functions

--- core/time/time.hpp
+++ core/time/time.hpp
@@ -1,23 +1,27 @@
 #pragma once
 
+#include <chrono>
+
 namespace SpaceEngine {
 
 class Time {
 public:
-  static void update(float currentTime);
+  static void init();
+  static void update();
   static void setTimeScale(float scale);
 
   static float deltaTime();
-  static float fixedDeltaTime();
   static float time();
   static float unscaledDeltaTime();
   static float timeScale();
 
 private:
-  static float s_currentTime;
-  static float s_lastFrameTime;
-  static float s_deltatTime;
+  static std::chrono::steady_clock::time_point s_startTime;
+  static std::chrono::steady_clock::time_point s_lastFrameTime;
+
   static float s_time;
+  static float s_deltaTime;
+  static float s_unscaledDeltaTime;
   static float s_timeScale;
 };
 
